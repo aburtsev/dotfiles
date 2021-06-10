0cbbdc3868a735939d1d222c0fa8ac78a32c390e
burtsev <aburcev@gmail.com>
2021-06-10 21:22:37 +0300

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,195 @@
+# set -U fish_user_paths $fish_user_paths $HOME/.local/bin/
+set fish_greeting                      # Supresses fish's intro message
+set TERM "xterm-256color"              # Sets the terminal type
+set EDITOR "emacsclient -t -a ''"      # $EDITOR use Emacs in terminal
+set VISUAL "emacsclient -c -a emacs"   # $VISUAL use Emacs in GUI mode
+
+
+### PROMPT ###
+# This was the 'sashimi' prompt from oh-my-fish.
+function fish_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o 98be65)
+  set -l yellow (set_color -o yellow)
+  set -g red (set_color -o 98be65)
+  set -g blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -g normal (set_color magenta)
+
+  set -l ahead (_git_ahead)
+  set -g whitespace ' '
+
+  if test $last_status = 0
+    set initial_indicator "$green◆"
+    set status_indicator "$normal❯$cyan❯$green❯"
+  else
+    set initial_indicator "$red✖ $last_status"
+    set status_indicator "$red❯$red❯$red❯"
+  end
+  set -l cwd $cyan(basename (prompt_pwd))
+
+  if [ (_git_branch_name) ]
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($red$git_branch$normal)"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($blue$git_branch$normal)"
+    end
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
+    end
+  end
+  # Notify if a command took more than 5 minutes
+  if [ "$CMD_DURATION" -gt 300000 ]
+    echo The last command took (math "$CMD_DURATION/1000") seconds.
+  end
+
+  echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
+end
+
+function _git_ahead
+  set -l commits (command git rev-list --left-right '@{upstream}...HEAD' ^/dev/null)
+  if [ $status != 0 ]
+    return
+  end
+  set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
+  set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
+  switch "$ahead $behind"
+    case ''     # no upstream
+    case '0 0'  # equal to upstream
+      return
+    case '* 0'  # ahead of upstream
+      echo "$blue↑$normal_c$ahead$whitespace"
+    case '0 *'  # behind upstream
+      echo "$red↓$normal_c$behind$whitespace"
+    case '*'    # diverged from upstream
+      echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
+  end
+end
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function fish_mode_prompt
+  switch $fish_bind_mode
+    case default
+      set_color --bold red
+      echo '(N) '
+    case insert
+      set_color --bold green
+      echo '(I) '
+    case replace_one
+      set_color --bold green
+      echo '(R) '
+    case visual
+      set_color --bold brmagenta
+      echo '(V) '
+    case '*'
+      set_color --bold red
+      echo '(?) '
+  end
+  set_color normal
+end
+### END OF PROMPT ###
+
+
+### DEFAULT EMACS MODE OR VI MODE ###
+# function fish_user_key_bindings
+#   fish_default_key_bindings
+#   # fish_vi_key_bindings
+# end
+### END OF VI MODE ###
+
+### FUNCTIONS ###
+
+# Functions needed for !! and !$
+# Will only work in default (emacs) mode.
+# Will NOT work in vi mode.
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+### ALIASES ###
+
+# vim and emacs
+alias vim='nvim'
+# alias em='/usr/bin/emacs -nw'
+# alias emacs="emacsclient -c -a 'emacs'"
+# alias doomsync="~/.emacs.d/bin/doom sync"
+# alias doomdoctor="~/.emacs.d/bin/doom doctor"
+# alias doomupgrade="~/.emacs.d/bin/doom upgrade"
+# alias doompurge="~/.emacs.d/bin/doom purge"
+
+# broot
+# alias br='broot -dhp'
+# alias bs='broot --sizes'
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# adding flags
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+# alias vifm='./.config/vifm/scripts/vifmrun'
+
+# alias merge='xrdb -merge ~/.Xresources'
+
+# get error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# switch between shells
+# alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+# alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+# alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+# bare git repo alias for dotfiles
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+alias fd=fdfind
+
+set -U fish_user_paths $HOME/.deno/bin $HOME/.nvm/versions/node/v14.16.0/bin $fish_user_paths
