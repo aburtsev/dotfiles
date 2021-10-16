7f1b0d422334d360658de9ad02ffc122c84cff3f
burtsev <aburcev@gmail.com>
2021-10-16 20:12:56 +0300

    fish. add alias to doom doctor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -71,9 +71,9 @@ end
 alias vim='nvim'
 # alias em='/usr/bin/emacs -nw'
 # alias emacs="emacsclient -c -a 'emacs'"
-alias doomsync="~/.emacs.d/bin/doom sync"
-# alias doomdoctor="~/.emacs.d/bin/doom doctor"
-alias doomupgrade="~/.emacs.d/bin/doom upgrade"
+alias doomsync="~/.doom-emacs.d/bin/doom sync"
+alias doomdoctor="~/.doom-emacs.d/bin/doom doctor"
+alias doomupgrade="~/.doom-emacs.d/bin/doom upgrade"
 # alias doompurge="~/.emacs.d/bin/doom purge"
 
 # broot
